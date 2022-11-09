b9b4a5629a37a51ab9d11620d9c6cc6c63f99145
chubachi <huyu_pc@outlook.jp>
2022-11-09 02:54:14 +0000

    ファイルアップロードページのルーディング

--- django_project/webapp/views.py
+++ django_project/webapp/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello world")
+    
+
+def uploadFile(request):
+    if request.method == "POST":
+        # Fetching the form data
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        # Saving the information in the database
+        document = models.Document(
+            title = fileTitle,
+            uploadedFile = uploadedFile
+        )
+        document.save()
+
+    documents = models.Document.objects.all()
+
+    return render(request, "webapp/upload.html", context = {
+        "files": documents
+    })
